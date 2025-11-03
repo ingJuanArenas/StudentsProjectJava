e5c39d8d22ee59fee3f3ec5484ccf384b388f755
ingJuanArenas <juanarenasing@gmail.com>
2025-11-03 15:00:52 -0500

    all working succesfully

--- src/App.java
+++ src/App.java
@@ -1,5 +1,40 @@
+import model.Student;
+import service.StudentService;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var salir = false;
+        StudentService.loadStudents();
+        do {
+           System.out.println("1. Listar estudiantes");
+            System.out.println("2. Agregar estudiante");
+            System.out.println("3. Calcular promedio");
+            System.out.println("4. Salir");
+            System.out.print("Opción: ");
+            var opcion = Integer.parseInt(System.console().readLine());
+            switch (opcion) {
+                case 1:
+                    StudentService.getStudents();
+                    break;
+                case 2:
+                    System.out.print("Nombre del estudiante: ");
+                    var name = System.console().readLine();
+                    System.out.print("Nota del estudiante: ");
+                    var grade = Double.parseDouble(System.console().readLine());
+                    StudentService.addStudent(new Student(name, grade));
+                    break;
+                case 3:
+                    StudentService.calculate_average();
+                    break;
+                case 4:
+                    StudentService.getOrderedStudents();
+                    break;
+                case 0:
+                    salir= true;
+                    break;
+                default:
+                    System.out.println("Opción inválida");
+            }
+        } while (salir != true);
     }
 }
